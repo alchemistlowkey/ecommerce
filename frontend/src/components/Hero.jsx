63a6b18da88c7edde57883ef9c717ec6a6a2924a
alchemistlowkey <alchemistlowkey@gmail.com>
2024-11-29 12:03:24 +0100

    project progress so far

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col sm:flex-row border border-gray-400">
+      {/* Hero Left side */}
+      <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
+        <div className="text-[#414141]">
+          <div className="flex items-center gap-2">
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+
+            <p className="text-sm font-medium md:text-base">OUR BESTSELLERS</p>
+          </div>
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+            Latest Arrivals
+          </h1>
+          <div className="flex items-center gap-2">
+            <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
+            <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+          </div>
+        </div>
+      </div>
+
+      {/* Hero Right side */}
+      <img src={assets.hero_img} alt="Hero" className="w-full sm:w-1/2" />
+    </div>
+  );
+};
+
+export default Hero;
