0607430b0d1e6f936b50bc4b308f1afdf3697ae7
alchemistlowkey <alchemistlowkey@gmail.com>
2024-12-06 17:11:18 +0100

    Admin: Add page still in progress

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -2,8 +2,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Login = () => {
+// eslint-disable-next-line react/prop-types
+const Login = ({setToken}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -14,8 +16,15 @@ const Login = () => {
         email,
         password
       });
-      console.log(response);
-    } catch (error) {}
+      if (response.data.success) {
+        setToken(response.data.token)
+      } else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+    }
   };
 
   return (
